39d63e13f15701fdfab96a87312532ab03dad1e4
emmgait <55083442+emmgait@users.noreply.github.com>
2019-09-10 12:46:47 +0300

    Changes rem to pixels

--- src/assets/js/changes.js
+++ src/assets/js/changes.js
@@ -2,13 +2,13 @@ const toIncrease= () =>{
 	const element = document.getElementById("toIncDecr");
 	const style = window.getComputedStyle(element,null).getPropertyValue('font-size');
 	const fontSize= parseFloat(style);
-	element.style.fontSize = (fontSize + 2) +'rem';
+	element.style.fontSize = (fontSize + 2) +'px';
 }
 const toDecrease= () =>{
 	const incDec = document.getElementById("toIncDecr");
 	const style = window.getComputedStyle(incDec,null).getPropertyValue('font-size');
 	const fontSize= parseFloat(style);
-	incDec.style.fontSize = (fontSize - 2) +'rem';
+	incDec.style.fontSize = (fontSize - 2) +'px';
 }
 
 const toggleClass= () =>{
